fa53d338faa51376f1c3563fbed0d1043d1a3fa5
Dimas Fendriansyah <dimasfendriansyah123098@gmail.com>
2021-01-29 10:29:18 +0700

    Initial Commit

---
+++ NavigationUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationUi
+//
+//  Created by dimas pendriansyah on 29/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
